568343c3525e47cc4d642864c1754cfce3cbcdcd
Cody <cody.king04@outlook.com>
2023-04-19 21:19:46 -0400

    Defined routes for express API

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+app.get("/post/:id", (req, res) => {
+
+});
+
+app.post("/post", (req, res) => {
+
+});
+
+app.put("/post/:id", (req, res) => {
+
+});
+
+app.delete("/post/:id", (req, res) => {
+
+});
+
+app.listen(3000);
