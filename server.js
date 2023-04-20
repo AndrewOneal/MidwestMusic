05699d4798e709954b8ccd571b170bbfbe7bcbf8
Cody <cody.king04@outlook.com>
2023-04-19 23:14:07 -0400

    POST working correctly

--- server.js
+++ server.js
@@ -1,18 +1,33 @@
 const { MongoClient } = require("mongodb");
 const express = require("express");
+const bodyParser = require("body-parser");
+const port = 3000;
 const uri = "mongodb+srv://codyking04:xbsYjbT03CcLrgen@ase220.hct6otj.mongodb.net/?retryWrites=true&w=majority";
 
 const app = express();
 const client = new MongoClient(uri);
+app.use(bodyParser.json());
 
-client.connect();
+async function connect() {
+    let connection = await client.connect();
+    return connection;
+}
+
+async function insert(db, database, collection, document) {
+    let dbo=db.db(database);
+    let result=await dbo.collection(collection).insertOne(document);
+    console.log(result);
+    return result;
+}
 
 app.get("/post/:id", (req, res) => {
 
 });
 
-app.post("/post", (req, res) => {
-
+app.post("/post", async (req, res) => {
+    console.log(req.body);
+	let result=await insert(db,'Assignment6','Posts',req.body);
+	res.json(result);
 });
 
 app.put("/post/:id", (req, res) => {
@@ -23,4 +38,12 @@ app.delete("/post/:id", (req, res) => {
 
 });
 
-app.listen(3000);
+async function start(){
+	db=await connect();
+	console.log('mongoDB connected');
+	app.listen(port,()=>{
+	  console.log(`App listening on port ${port}`);
+	});
+}
+
+start();
