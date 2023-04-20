d3ce3c7865b536133bc86282dde014cbb04b6a39
Cody King <cody.king04@outlook.com>
2023-04-20 10:41:21 -0400

    Added comments

--- server.js
+++ server.js
@@ -11,30 +11,36 @@ app.use(bodyParser.json());
 app.use(cors());
 
 async function connect() {
+    // Connecting to atlas database.
     let connection = await client.connect();
     return connection;
 }
 
 async function find(db, database, collection, criteria) {
+    // Fetching database and setting it to dbo.
     let dbo = db.db(database);
+    // Finding documents that match criteria from the collection and returning result as array.
     let result = await dbo.collection(collection).find(criteria).toArray();
     return result;
 }
 
 async function insert(db, database, collection, document) {
+    // Fetching database and setting it to dbo.
     let dbo=db.db(database);
+    // Inserting new document into collection and returning result.
     let result=await dbo.collection(collection).insertOne(document);
     console.log(result);
     return result;
 }
 
 app.get("/post", async (req, res) => {
+    // Requesting the Posts collection and sending it back in the response.
     let result = await find(db, "Assignment6", "Posts", {});
     res.json(result);
 });
 
 app.post("/post", async (req, res) => {
-    console.log(req.body);
+    // Inserting document into Posts collection and responding with result of request.
 	let result=await insert(db,'Assignment6','Posts',req.body);
 	res.json(result);
 });
@@ -48,9 +54,9 @@ app.delete("/post/:id", (req, res) => {
 });
 
 async function start(){
-	db=await connect();
+	db = await connect();
 	console.log('mongoDB connected');
-	app.listen(port,()=>{
+	app.listen(port,() => {
 	  console.log(`App listening on port ${port}`);
 	});
 }
