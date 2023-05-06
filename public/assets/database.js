af0f088d362a5f403e9e783aa7aa57371cbe1a87
Cody <cody.king04@outlook.com>
2023-05-05 22:42:01 -0400

    Deleted leftover commented out code

--- public/assets/database.js
+++ public/assets/database.js
@@ -16,24 +16,12 @@ const database={
 		});
 	},
 	delete:function(index){
-		// api.GET(function(response){
-		// 	response.data.splice(index,1);
-		// 	api.PUT(response.data,function(){
-		// 		alert('The post has been deleted. Please go back to the home page');
-		// 	});
-		// });
 		api.REMOVE(index,function(){
 			alert('The post has been deleted.');
 			document.location.href = "/templates";
 		})
 	},
 	create:function(newData){
-		// api.GET(function(response){
-		// 	response.data.push(newData);
-		// 	api.PUT(response.data,function(){
-		// 		alert('The post has been added. Please go back to the home page');
-		// 	});
-		// });
 		api.POST(newData,function(){
 			alert('The post has been added.');
 			document.location.href = "/templates";
